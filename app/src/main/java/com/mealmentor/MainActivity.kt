fbd07410e414284e6d52e44863bbdb35c529db17
Andrii <andriymvnaruto@gmail.com>
2024-02-02 23:25:38 +0200

    Створено репозиторій

---
+++ app/src/main/java/com/mealmentor/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.mealmentor
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.mealmentor.ui.theme.MealMentorKotlinTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MealMentorKotlinTheme {
+
+            }
+        }
+    }
+}
