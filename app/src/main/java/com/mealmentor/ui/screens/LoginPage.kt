e3b0bb28491e92fa50e301872f54b35d6c42bf13
Andrii <andriymvnaruto@gmail.com>
2024-02-04 12:07:26 +0200

    оновлено вікно авторизації

--- app/src/main/java/com/mealmentor/ui/screens/LoginPage.kt
+++ app/src/main/java/com/mealmentor/ui/screens/LoginPage.kt
@@ -4,7 +4,9 @@ import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +15,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -44,95 +48,38 @@ fun LoginPage() {
         mutableStateOf("")
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.primary),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-
-        // Виклик функцій для відображення текстових полів
-        EmailField(
-            text = emailText.value,
-            onValueChange = {
-                emailText.value = it
-            }
-        )
-
-        Spacer(modifier = Modifier.padding(vertical = 8.dp))
-
-        PasswordField(
-            text = passwordText.value,
-            onValueChange = {
-                passwordText.value = it
-            }
-        )
-
-        Spacer(modifier = Modifier.padding(vertical = 5.dp))
-
-        Text(
-            text = stringResource(id = R.string.forgot_password),
-            textAlign = TextAlign.Center,
-            color = MaterialTheme.colorScheme.onPrimary,
-            style = MaterialTheme.typography.labelMedium,
+    Box (
+        modifier = Modifier.fillMaxSize()
+    ){
+        Column(
             modifier = Modifier
-                .padding(start = 5.dp)
-                .clickable {
-                    Toast
-                        .makeText(
-                            context,
-                            "Forgot Password",
-                            Toast.LENGTH_SHORT
-                        )
-                        .show()
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.primary),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            EmailField(
+                text = emailText.value,
+                onValueChange = {
+                    emailText.value = it
                 }
-        )
-
-        Spacer(modifier = Modifier.padding(vertical = 5.dp))
-
-        ElevatedButton(
-            onClick = {
-                Toast.makeText(
-                    context,
-                    "Login",
-                    Toast.LENGTH_SHORT
-                ).show()
-            },
-            shape = ButtonDefaults.elevatedShape,
-            colors = ButtonDefaults.elevatedButtonColors(
-                containerColor = MaterialTheme.colorScheme.tertiary,
-                contentColor = MaterialTheme.colorScheme.onTertiary
-            ),
-            elevation = ButtonDefaults.elevatedButtonElevation(
-                defaultElevation = 4.dp,
-                pressedElevation = 0.dp
-            ),
-            modifier = Modifier
-                .padding(horizontal = 80.dp)
-                .height(65.dp)
-                .fillMaxWidth()
-        ) {
-            Text(
-                text = stringResource(id = R.string.login),
-                style = MaterialTheme.typography.labelLarge
-            )
-        }
-
-        Spacer(modifier = Modifier.padding(vertical = 5.dp))
-
-        Row(
-            horizontalArrangement = Arrangement.Center
-        ) {
-
-            Text(
-                text = stringResource(id = R.string.dont_have_account),
-                color = MaterialTheme.colorScheme.onPrimary,
-                style = MaterialTheme.typography.labelSmall
             )
 
+            Spacer(modifier = Modifier.padding(vertical = 8.dp))
+
+            PasswordField(
+                text = passwordText.value,
+                onValueChange = {
+                    passwordText.value = it
+                }
+            )
+
+            Spacer(modifier = Modifier.padding(vertical = 5.dp))
+
             Text(
-                text = stringResource(id = R.string.sign_up),
+                text = stringResource(id = R.string.forgot_password),
+                textAlign = TextAlign.Center,
                 color = MaterialTheme.colorScheme.onPrimary,
                 style = MaterialTheme.typography.labelMedium,
                 modifier = Modifier
@@ -141,53 +88,128 @@ fun LoginPage() {
                         Toast
                             .makeText(
                                 context,
-                                "Sign Up",
+                                "Forgot Password",
                                 Toast.LENGTH_SHORT
                             )
                             .show()
                     }
             )
+
+            Spacer(modifier = Modifier.padding(vertical = 5.dp))
+
+            ElevatedButton(
+                onClick = {
+                    Toast.makeText(
+                        context,
+                        "Login",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                },
+                shape = ButtonDefaults.elevatedShape,
+                colors = ButtonDefaults.elevatedButtonColors(
+                    containerColor = MaterialTheme.colorScheme.tertiary,
+                    contentColor = MaterialTheme.colorScheme.onTertiary
+                ),
+                elevation = ButtonDefaults.elevatedButtonElevation(
+                    defaultElevation = 4.dp,
+                    pressedElevation = 0.dp
+                ),
+                modifier = Modifier
+                    .padding(horizontal = 80.dp)
+                    .height(65.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = stringResource(id = R.string.login),
+                    style = MaterialTheme.typography.labelLarge
+                )
+            }
+
+            Spacer(modifier = Modifier.padding(vertical = 5.dp))
+
+            Row(
+                horizontalArrangement = Arrangement.Center
+            ) {
+
+                Text(
+                    text = stringResource(id = R.string.dont_have_account),
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    style = MaterialTheme.typography.labelSmall
+                )
+
+                Text(
+                    text = stringResource(id = R.string.sign_up),
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    style = MaterialTheme.typography.labelMedium,
+                    modifier = Modifier
+                        .padding(start = 5.dp)
+                        .clickable {
+                            Toast
+                                .makeText(
+                                    context,
+                                    "Sign Up",
+                                    Toast.LENGTH_SHORT
+                                )
+                                .show()
+                        }
+                )
+            }
+
         }
 
-    }
-
-    // Відображення тексту "Meal Mentor"
-    Text(
-        text = "Meal Mentor",
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 50.dp),
-        textAlign = TextAlign.Center,
-        color = MaterialTheme.colorScheme.onPrimary,
-        style = MaterialTheme.typography.titleLarge
-    )
-
-    // Кнопка "Увійти з Google"
-    ElevatedButton(
-        onClick = {
-            Toast.makeText(
-                context,
-                "Google Button",
-                Toast.LENGTH_SHORT
-            ).show()
-        },
-        shape = ButtonDefaults.elevatedShape,
-        colors = ButtonDefaults.elevatedButtonColors(
-            containerColor = MaterialTheme.colorScheme.secondary,
-            contentColor = MaterialTheme.colorScheme.onSecondary
-        ),
-        elevation = ButtonDefaults.elevatedButtonElevation(
-            defaultElevation = 4.dp,
-            pressedElevation = 0.dp
-        ),
-        modifier = Modifier
-            .padding(horizontal = 16.dp)
-            .height(65.dp)
-            .fillMaxWidth()
-    ) {
+        // Відображення тексту "Meal Mentor"
         Text(
-            text = stringResource(id = R.string.login_with_google),
-            style = MaterialTheme.typography.labelLarge
+            text = "Meal Mentor",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 50.dp),
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.onPrimary,
+            style = MaterialTheme.typography.titleLarge
         )
+
+        // Кнопка "Увійти з Google"
+        ElevatedButton(
+            onClick = {
+                Toast.makeText(
+                    context,
+                    "Google Button",
+                    Toast.LENGTH_SHORT
+                ).show()
+            },
+            shape = ButtonDefaults.elevatedShape,
+            colors = ButtonDefaults.elevatedButtonColors(
+                containerColor = MaterialTheme.colorScheme.secondary,
+                contentColor = MaterialTheme.colorScheme.onSecondary
+            ),
+            elevation = ButtonDefaults.elevatedButtonElevation(
+                defaultElevation = 4.dp,
+                pressedElevation = 0.dp
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+                .height(65.dp)
+                .align(Alignment.BottomCenter),
+            contentPadding = PaddingValues(start = 16.dp, top = 10.dp, end = 16.dp, bottom = 10.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.google),
+                    contentDescription = "google_img",
+                    tint = MaterialTheme.colorScheme.onSecondary
+                )
+                Text(
+                    text = stringResource(id = R.string.login_with_google),
+                    style = MaterialTheme.typography.labelLarge,
+                    modifier = Modifier.padding(start = 16.dp)
+                )
+            }
+        }
     }
+
 }
