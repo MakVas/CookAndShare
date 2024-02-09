28f0da473d8a3768bc6b557d0acc1ae175de06f9
Andrii <andriymvnaruto@gmail.com>
2024-02-10 00:31:21 +0200

    з'явилась заготовка для майбутнього RecipeItem, невелике оновлення скріна home

--- app/src/main/java/com/mealmentor/ui/pages/screens/main/HomeScreen.kt
+++ app/src/main/java/com/mealmentor/ui/pages/screens/main/HomeScreen.kt
@@ -2,9 +2,11 @@ package com.mealmentor.ui.pages.screens.main
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.LazyRow
@@ -13,7 +15,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.mealmentor.R
+import com.mealmentor.ui.pages.screens.elements.RecipeColumnItem
+import com.mealmentor.ui.pages.screens.elements.RecipeRowItem
 
 @Composable
 fun HomeScreen() {
@@ -34,10 +40,30 @@ fun NestedScrolling() {
     LazyColumn(
         modifier = Modifier
             .fillMaxSize(),
-        contentPadding = PaddingValues(16.dp)
+        //contentPadding = PaddingValues(16.dp)
     ) {
         item {
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                modifier = Modifier.padding(start = 16.dp),
+                text = stringResource(id = R.string.top_of_the_day),
+                style = MaterialTheme.typography.titleLarge
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
             SubRow()
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                modifier = Modifier.padding(start = 16.dp),
+                text = stringResource(id = R.string.subscribed_recipes),
+                style = MaterialTheme.typography.titleLarge
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
         }
         subColumn()
     }
@@ -46,14 +72,16 @@ fun NestedScrolling() {
 @Composable
 fun SubRow() {
     LazyRow {
-        items(100) {
-            Text(text = "Item $it")
+        items(50) {
+            Spacer(modifier = Modifier.width(16.dp))
+            RecipeRowItem(text = "Item $it")
         }
     }
 }
 
 fun LazyListScope.subColumn() {
-    items(100) {
-        Text(text = "Item $it")
+    items(50) {
+        RecipeColumnItem(text = "Item $it")
+        Spacer(modifier = Modifier.height(16.dp))
     }
 }
